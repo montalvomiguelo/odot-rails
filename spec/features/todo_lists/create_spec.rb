ee5740ce65f75ebb5bcf4a56a8070550ec5e95c1
imontalvomiguel <ubaldo.im@gmail.com>
2016-06-05 15:53:50 -0500

    add test to have an error when description is less than 5 length

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -60,4 +60,20 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("Hello")
   end
+
+  it "displays an error when todo list has a description less than 5 characters" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hello"
+    fill_in "Description", with: "Hi"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Hello")
+  end
 end
