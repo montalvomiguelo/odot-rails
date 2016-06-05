c63d832b3bf890efa12f731c608feae9ad2eebaf
imontalvomiguel <ubaldo.im@gmail.com>
2016-06-05 15:30:11 -0500

    write tests

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: "This is what I'm doing today"
+    click_button "Create Todo list"
+    expect(page).to have_content("Todo list was successfully created")
+  end
+
+  it "displays an error when the tdo list has no title" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is what I'm doing today"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today")
+  end
+end
