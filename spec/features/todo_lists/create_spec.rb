bdca4e48754747073eb7ecb412fac2feab1d85a3
imontalvomiguel <ubaldo.im@gmail.com>
2016-06-05 15:49:38 -0500

    add test for description error when empty

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -44,4 +44,20 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is what I'm doing today")
   end
+
+  it "displays an error when the todo list has no description" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hello"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Hello")
+  end
 end
