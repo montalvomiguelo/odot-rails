59a0d501b678f0b2d2c86093f1976477aecea57c
imontalvomiguel <ubaldo.im@gmail.com>
2016-06-10 14:21:12 -0500

    add test to validate todo_items data

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,4 @@
 class TodoItem < ActiveRecord::Base
+  validates :content, presence: true, length: { minimum: 3 }
   belongs_to :todo_list
 end
--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -10,15 +10,30 @@ describe "Adding todo items" do
     end
   end
 
-  it "is successful with valid content" do
+  def create_todo_item(options={})
+    options[:content] ||= "My todo item"
     visit_todolist(todo_list)
     click_link "New Todo Item"
-    fill_in "Content", with: "Milk"
+    fill_in "Content", with: options[:content]
     click_button "Save"
+  end
+
+  it "is successful with valid content" do
+    create_todo_item(content: "Milk")
     expect(page).to have_content("Todo item was successfully created.")
     within("ul.todo_items") do
       expect(page).to have_content("Milk")
     end
+  end
+
+  it "displays an error with no content" do
+    create_todo_item(content: "")
+    expect(page).to have_content("error")
+  end
+
+  it "displays an error with content less than 3 characters long" do
+    create_todo_item(content: "hi")
+    expect(page).to have_content("error")
   end
 
 end
